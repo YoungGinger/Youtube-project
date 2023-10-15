f76133630d0dc34b6f638c5d69ded405a48dbe08
YoungGinger <105806245+YoungGinger@users.noreply.github.com>
2023-10-15 21:48:20 +0200

    wip

---
+++ scripts/YouTube-js.js
@@ -0,0 +1,13 @@
+const hamburgerMenu = document.querySelector('.js-hamburger-container');
+const hamburgerMenu1 = document.querySelector('.js-hamburger-container1');
+let click;
+
+hamburgerMenu.addEventListener('click', function() { 
+    const sidebar = document.querySelector('.js-sidebar');
+    sidebar.classList.toggle('show');
+});
+
+hamburgerMenu1.addEventListener('click', function() { 
+  const sidebar = document.querySelector('.js-sidebar');
+  sidebar.classList.remove('show');
+});
